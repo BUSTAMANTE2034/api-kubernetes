1960b1de0fea1ac3b09371b19b882d1dfbe87129
BUSTAMANTE2034 <bustamanteservincarloseduardo@gmail.com>
2024-10-09 11:48:13 -0600

    Creación inicial de la estructura del API

---
+++ API/config .py
@@ -0,0 +1,5 @@
+import os
+
+class config:
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://Lalo2034:Ing.Bustaduar.mysql.pythonanywhere-services.com:3306/Lalo2034$AC' #Conectar a la base de datos
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
